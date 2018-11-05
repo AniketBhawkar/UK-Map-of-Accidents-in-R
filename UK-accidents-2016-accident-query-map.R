3efd0a6d145f2f3555a40fc08fae9fdba1cb4bac
AniketBhawkar <aniketbhawkar@yahoo.com>
2018-11-05 14:36:23 +0000

    Add files via upload

---
+++ UK-accidents-2016-accident-query-map.R
@@ -0,0 +1,12 @@
+# Set Working Directory
+setwd("C:/Users/abc/Documents/dataset/")
+
+# Load Data of 2016
+accident.details2016 = read.csv("uk-2016-road-safety-data/dftRoadSafety_Accidents_2016.csv")
+
+accident.fullData2016 = accident.details2016
+
+library(rworldmap)
+newmap <- getMap(resolution = "low")
+plot(newmap, xlim = c(-5, 5), ylim = c(50, 60), asp = 1)
+points(accident.fullData2016$Longitude, accident.fullData2016$Latitude, col = "red", cex = .05)
